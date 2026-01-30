2ebc971f0a071bdcb06ce74f8bcc1b5b77a841de
Ethan Decleyn <ethandecleyn@gmail.com>
2026-01-30 17:41:43 +0100

    wip

--- feature_tracker/src/feature_tracker.rs
+++ feature_tracker/src/feature_tracker.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 use image::{self, GrayImage, ImageBuffer, Luma, Pixel, imageops::*};
 
 type ImageFloat = f32;
+type Pyramid = Vec<ImageBuffer<Luma<ImageFloat>, Vec<ImageFloat>>>;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FeatureTrackingConfig {
@@ -12,22 +13,44 @@ pub struct FeatureTrackingConfig {
 
 
 pub struct FeatureTracker {
-    config: FeatureTrackingConfig
+    config: FeatureTrackingConfig,
+    previous_pyramid: Option<Pyramid>
 }
 
-fn build_image_pyramid(image: &GrayImage, nlevels: usize, ratio: f64) {
+impl FeatureTracker {
+    pub fn new(config: FeatureTrackingConfig) -> Self {
+        FeatureTracker { config, previous_pyramid: None }
+    }
+
+    pub fn process_frame(&mut self, image: &GrayImage) 
+    {
+        let pyramid = build_image_pyramid(image, self.config.nlevels, self.config.ratio);
+        if let Some(previous_pyramid) = self.previous_pyramid.as_ref() {
+            println!("there was a previous pyramid");
+        };
+
+        self.previous_pyramid = Some(pyramid);
+    }
+
+    pub fn get_pyramid(&self) -> Option<&Pyramid> {
+        self.previous_pyramid.as_ref()
+    }
+}
+
+
+fn build_image_pyramid(image: &GrayImage, nlevels: usize, ratio: f64) -> Pyramid {
     let image: ImageBuffer<Luma<ImageFloat>, Vec<_>> = ImageBuffer::from_vec(
         image.width(), 
         image.height(),
         image.pixels()
-            .map(|p| p.channels()[0] as ImageFloat)
+            .map(|p| (p.channels()[0] as ImageFloat) / 255.0)
             .collect::<Vec<_>>()
     ).unwrap();
     let (w0, h0) = image.dimensions();
 
     let mut pyramid = Vec::new();
     pyramid.push(image);
-    for l in 0..nlevels {
+    for l in 1..nlevels {
 
         let level = i32::try_from(l).expect("Overflow caused by number of pyramid levels superior to i32::MAX. Consider decreasing number of pyramid levels");
         let nwidth = (f64::try_from(w0).unwrap() / ratio.powi(level)).round() as u32;
@@ -36,18 +59,24 @@ fn build_image_pyramid(image: &GrayImage, nlevels: usize, ratio: f64) {
         let previous_image = pyramid.last().unwrap();
         pyramid.push(resize(previous_image, nwidth, nheight, Triangle));
     } 
-}
-
-impl FeatureTracker {
-    pub fn new(config: FeatureTrackingConfig) -> Self {
-        FeatureTracker { config }
-    }
-
-    pub fn process_frame<P: Pixel>(&self, image: &GrayImage) 
-    {
-        let pyramid = build_image_pyramid(image, self.config.nlevels, self.config.ratio);
-        
-    }
+    pyramid
 }
 
 
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_image_pyramid() {
+        let (w, h) = (120, 60);
+        let gray_image = GrayImage::from_vec(w, h, 
+            Vec::from_iter(std::iter::repeat(5).take((w*h) as usize))).unwrap();
+
+        let pyramid = build_image_pyramid(&gray_image, 3, 2.0);
+        assert!(pyramid[0].dimensions() == (120, 60));
+        assert!(pyramid[1].dimensions() == (60, 30));
+        assert!(pyramid[2].dimensions() == (30, 15));
+    }
+}
--- feature_tracker/src/players/tartanair_player.rs
+++ feature_tracker/src/players/tartanair_player.rs
@@ -4,7 +4,7 @@ use std::thread;
 use std::time;
 
 use anyhow::Result;
-use image;
+use image::{self, ImageBuffer, Luma};
 use rerun as rr;
 
 use crate::feature_tracker::*;
@@ -21,7 +21,7 @@ impl TartanAirPlayer {
     }
 
     pub fn run(&self, config: FeatureTrackingConfig) -> Result<()> {
-        let tracker = FeatureTracker::new(config);
+        let mut tracker = FeatureTracker::new(config);
         
         let rec = rr::RecordingStreamBuilder::new("Patch Tracker").spawn()?;
         let left_entity_path = "image_left";
@@ -31,12 +31,14 @@ impl TartanAirPlayer {
             .collect();
         left_images.sort_by_key(|dir_entry| dir_entry.path());
         
-        for file in left_images.iter().skip(300).take(200) {
-            println!("{file:?}");
+        for file in left_images.iter().skip(300).take(100) {
             let left_image = image::open(file.path())?;
 
-            rec.log(left_entity_path, &rr::Image::from_dynamic_image(left_image)?)?;
-            thread::sleep(time::Duration::from_millis(50));
+            tracker.process_frame(&left_image.to_luma8());
+            rec.log(
+                left_entity_path,
+                &rr::Image::from_dynamic_image(left_image)?
+            )?;
         }
         
 
